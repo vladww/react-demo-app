38a4414b6c9ad8c8c9e7a17c35dc0f1ba8d4c303
vladww <wonkaw@yandex.ru>
2021-04-13 18:40:37 +0300

    main files download 4

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App'
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
